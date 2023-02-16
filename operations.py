8f04165d412d61251ccbc909e03e55d9916434da
H0xC0000005 <zh0001hu@e.ntu.edu.sg>
2023-02-16 18:11:46 +0800

    trim hamberg

--- operations.py
+++ operations.py
@@ -1,4 +1,4 @@
 from cityscape_dataset import CityscapeDataset
 
-CityscapeDataset.trim_suffix_for_folder("/data/berlin_set/mask/")
+CityscapeDataset.trim_suffix_for_folder("./data/hamberg_set/image/")
 
