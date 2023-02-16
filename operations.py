f7142bc58f8dd0ae7322ada9e8ca1883579db265
H0xC0000005 <74247664+H0xC0000005@users.noreply.github.com>
2023-02-16 21:28:25 +0800

    refactor

--- operations.py
+++ operations.py
@@ -1,4 +1,4 @@
 from cityscape_dataset import CityscapeDataset
 
-CityscapeDataset.trim_suffix_for_folder("./data/hamberg_set/image/")
+CityscapeDataset.trim_suffix_for_folder("data/hamburg_set/image/")
 
