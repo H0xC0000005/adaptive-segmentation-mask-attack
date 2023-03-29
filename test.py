a43ef560440cc715e2d3f17cc97735a9b8152781
H0xC0000005 <zh0001hu@e.ntu.edu.sg>
2023-03-29 16:49:44 +0800

    l1ln update 4

--- test.py
+++ test.py
@@ -6,13 +6,18 @@ import torch
 m1 = np.array([[0, 0, 1, 1, ],
                [0, 0, 1, 1, ],
                [0, 0, 1, 1, ],
-               [0, 0, 1, 1, ]])
+               [0, 0, 1, 1, ]], dtype=float)
 m2 = np.array([[0, 7, 1, 1, ],
                [0, 5, 1, 1, ],
                [0, 4, 0, 0, ],
-               [2, 1, 1, 1, ]])
+               [0, 1, 1, 1, ]], dtype=float)
+mo = np.array([[1, 7, 1, 1, ],
+               [0, 5, 1, 1, ],
+               [0, 4, 0, 0, ],
+               [0, 1, 1, 1, ]], dtype=float)
 
 t1 = torch.from_numpy(m1)
 t2 = torch.from_numpy(m2)
-ext = evaluate_externality(t1, t2, 0, evaluation_mask=None)
+to = torch.from_numpy(mo)
+ext = evaluate_externality(t1, to, t2, 0, evaluation_mask=None)
 print(ext)
